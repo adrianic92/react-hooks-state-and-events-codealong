78fea015f11f1d9b803f400d6b1449b51aab9a7b
Adrian <adrianic92@yahoo.com>
2022-09-20 11:20:07 -0700

    updated component

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+  const [toggle, setToggle] = useState("OFF")
+
+  let color = "white"
+
+  function toggler() {
+    setToggle(toggler => (toggler === "OFF") ? (toggler = "ON") : toggler = "OFF")
+  }
+
+  if (toggle === "ON") {
+    color = "red"
+  }
+  else {
+    color = "white"
+  }
+
+  return <button onClick={toggler} style={{background: color}}>{toggle}</button>;
 }
 
 export default Toggle;
